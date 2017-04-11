f59286da81f12e3ea8f40a0d61a2d37835d714f6
David Kimaru <kimarudg@gmail.com>
2017-04-11 12:08:00 +0300

    Add Provider

---
+++ app/src/main/java/com/expansion/lg/kimaru/expansion/service/ExpansionContentProvider.java
@@ -0,0 +1,50 @@
+package com.expansion.lg.kimaru.expansion.service;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+/**
+ * Created by kimaru on 4/11/17.
+ */
+
+
+public class ExpansionContentProvider extends ContentProvider {
+
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        return null;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int bulkInsert(Uri uri, ContentValues[] values) {
+        return 0;
+    }
+
+}
