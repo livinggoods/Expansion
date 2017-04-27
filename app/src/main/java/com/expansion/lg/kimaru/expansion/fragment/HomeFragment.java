5c4d34e25251dffb0bb568e0bf272fb7657a8b27
David Kimaru <kimarudg@gmail.com>
2017-04-27 13:13:52 +0300

    Improve dashboard design

--- app/src/main/java/com/expansion/lg/kimaru/expansion/fragment/HomeFragment.java
+++ app/src/main/java/com/expansion/lg/kimaru/expansion/fragment/HomeFragment.java
@@ -3,15 +3,23 @@ package com.expansion.lg.kimaru.expansion.fragment;
  * Created by kimaru on 3/11/17.
  */
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.expansion.lg.kimaru.expansion.R;
+import com.expansion.lg.kimaru.expansion.activity.HttpServerActivity;
 import com.expansion.lg.kimaru.expansion.activity.MainActivity;
+import com.expansion.lg.kimaru.expansion.activity.SessionManagement;
+
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,7 +29,7 @@ import com.expansion.lg.kimaru.expansion.activity.MainActivity;
  * Use the {@link HomeFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,6 +38,12 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Handler mHandler;
+
+    Button mapping, recruitments, registrations, exams, interviews, sharing;
+    SessionManagement sessionManagement;
+    HashMap <String, String> user;
+    String country;
 
     private OnFragmentInteractionListener mListener;
 
@@ -70,7 +84,28 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         MainActivity.CURRENT_TAG = MainActivity.TAG_HOME;
         MainActivity.backFragment = null;
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        sessionManagement = new SessionManagement(getContext());
+        user = sessionManagement.getUserDetails();
+        country = sessionManagement.getUserDetails().get(SessionManagement.KEY_USER_COUNTRY);
+
+        //mapping, recruitments, registrations, exams, interviews, sharing
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        mapping = (Button) v.findViewById(R.id.btnMapping);
+        recruitments = (Button) v.findViewById(R.id.btnRecruitments);
+        registrations = (Button) v.findViewById(R.id.btnRegistrations);
+        exams = (Button) v.findViewById(R.id.btnExams);
+        interviews = (Button) v.findViewById(R.id.btnInterviews);
+        sharing = (Button) v.findViewById(R.id.btnSharing);
+
+        mapping.setOnClickListener(this);
+        recruitments.setOnClickListener(this);
+        registrations.setOnClickListener(this);
+        exams.setOnClickListener(this);
+        interviews.setOnClickListener(this);
+        sharing.setOnClickListener(this);
+
+
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -95,6 +130,58 @@ public class HomeFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onClick(View view){
+        Fragment fragment;
+        FragmentTransaction fragmentTransaction;
+        switch (view.getId()){
+            case R.id.btnMapping:
+                fragment = new MappingFragment();
+                fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, MainActivity.TAG_NEW_RECRUITMENT);
+                fragmentTransaction.commitAllowingStateLoss();
+                break;
+            case R.id.btnRecruitments:
+                fragment = new RecruitmentsFragment();
+                fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, MainActivity.TAG_NEW_RECRUITMENT);
+                fragmentTransaction.commitAllowingStateLoss();
+                break;
+            case R.id.btnRegistrations:
+                fragment = new RegistrationsFragment();
+                fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, MainActivity.TAG_NEW_RECRUITMENT);
+                fragmentTransaction.commitAllowingStateLoss();
+                break;
+            case R.id.btnExams:
+                fragment = new ExamsFragment();
+                fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, MainActivity.TAG_NEW_RECRUITMENT);
+                fragmentTransaction.commitAllowingStateLoss();
+                break;
+            case R.id.btnInterviews:
+                fragment = new InterviewsFragment();
+                fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, MainActivity.TAG_NEW_RECRUITMENT);
+                fragmentTransaction.commitAllowingStateLoss();
+                break;
+            case R.id.btnSharing:
+                startActivity(new Intent(getActivity(), HttpServerActivity.class));
+                break;
+
+        }
     }
 
     /**
