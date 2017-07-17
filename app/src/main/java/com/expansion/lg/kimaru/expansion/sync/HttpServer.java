8771fc1f3de505e0a08f3b48c96e4907264078e6
David Kimaru <kimarudg@gmail.com>
2017-07-17 16:39:19 +0300

    Allow server to receive all synced data

--- app/src/main/java/com/expansion/lg/kimaru/expansion/sync/HttpServer.java
+++ app/src/main/java/com/expansion/lg/kimaru/expansion/sync/HttpServer.java
@@ -8,11 +8,15 @@ import com.expansion.lg.kimaru.expansion.mzigos.Interview;
 import com.expansion.lg.kimaru.expansion.mzigos.Recruitment;
 import com.expansion.lg.kimaru.expansion.mzigos.Registration;
 import com.expansion.lg.kimaru.expansion.other.Constants;
+import com.expansion.lg.kimaru.expansion.tables.ChewReferralTable;
+import com.expansion.lg.kimaru.expansion.tables.CommunityUnitTable;
 import com.expansion.lg.kimaru.expansion.tables.EducationTable;
 import com.expansion.lg.kimaru.expansion.tables.ExamTable;
 import com.expansion.lg.kimaru.expansion.tables.InterviewTable;
+import com.expansion.lg.kimaru.expansion.tables.LinkFacilityTable;
 import com.expansion.lg.kimaru.expansion.tables.RecruitmentTable;
 import com.expansion.lg.kimaru.expansion.tables.RegistrationTable;
+import com.expansion.lg.kimaru.expansion.tables.SubCountyTable;
 import com.expansion.lg.kimaru.expansion.tables.UserTable;
 import com.koushikdutta.async.AsyncServer;
 import com.koushikdutta.async.http.AsyncHttpClient;
@@ -51,6 +55,10 @@ public class HttpServer {
     public static final String REGISTRATION_URL = "registrations";
     public static final String EXAM_URL = "exams";
     public static final String INTERVIEW_URL = "interviews";
+    public static final String LINKFACILITY_URL = "link_facilities";
+    public static final String CHEW_REFERRAL_URL = "chew_referral";
+    public static final String CU_URL = "community_unit";
+    public static final String SUBCOUNTY_URL = "subcounty";
     public static String url;
     Context context;
 
@@ -87,6 +95,38 @@ public class HttpServer {
                 response.send(interviewTable.getInterviewJson());
             }
         });
+        //// postCommunityUnit postChewReferral postLinkFacilities postSubCounties
+        server.get("/"+SUBCOUNTY_URL, new HttpServerRequestCallback() {
+            @Override
+            public void onRequest(AsyncHttpServerRequest request, AsyncHttpServerResponse response) {
+                SubCountyTable subCountyTable = new SubCountyTable(context);
+                response.send(subCountyTable.getJson());
+            }
+        });
+
+        server.get("/"+CU_URL, new HttpServerRequestCallback() {
+            @Override
+            public void onRequest(AsyncHttpServerRequest request, AsyncHttpServerResponse response) {
+                CommunityUnitTable communityUnitTable = new CommunityUnitTable(context);
+                response.send(communityUnitTable.getJson());
+            }
+        });
+        server.get("/"+CHEW_REFERRAL_URL, new HttpServerRequestCallback() {
+            @Override
+            public void onRequest(AsyncHttpServerRequest request, AsyncHttpServerResponse response) {
+                ChewReferralTable chewReferralTable = new ChewReferralTable(context);
+                response.send(chewReferralTable.getChewReferralJson());
+            }
+        });
+        server.get("/"+LINKFACILITY_URL, new HttpServerRequestCallback() {
+            @Override
+            public void onRequest(AsyncHttpServerRequest request, AsyncHttpServerResponse response) {
+                LinkFacilityTable linkFacilityTable = new LinkFacilityTable(context);
+                response.send(linkFacilityTable.getJson());
+            }
+        });
+        //
+
         server.get("/users", new HttpServerRequestCallback() {
             @Override
             public void onRequest(AsyncHttpServerRequest request, AsyncHttpServerResponse response) {
@@ -176,8 +216,126 @@ public class HttpServer {
                 }
             }
         });
+        //postCommunityUnit postChewReferral postLinkFacilities postSubCounties
+        server.post("/"+CU_URL, new HttpServerRequestCallback() {
+            @Override
+            public void onRequest(AsyncHttpServerRequest request, final AsyncHttpServerResponse response) {
+                try {
+                    assertNotNull(request.getHeaders().get("Host"));
+                    JSONObject json = new JSONObject();
+                    if (request.getBody() instanceof JSONObjectBody) {
+                        json = ((JSONObjectBody)request.getBody()).get();
+                        processCus(json);
+                    }
+                    response.send(json);
+                    // response.send(typeReceived);
+                }
+                catch (Exception e) {
+                    response.send(e.getMessage());
+                }
+            }
+        });
+
+        server.post("/"+CHEW_REFERRAL_URL, new HttpServerRequestCallback() {
+            @Override
+            public void onRequest(AsyncHttpServerRequest request, final AsyncHttpServerResponse response) {
+                try {
+                    assertNotNull(request.getHeaders().get("Host"));
+                    JSONObject json = new JSONObject();
+                    if (request.getBody() instanceof JSONObjectBody) {
+                        json = ((JSONObjectBody)request.getBody()).get();
+                        processChewReferral(json);
+                    }
+                    response.send(json);
+                    // response.send(typeReceived);
+                }
+                catch (Exception e) {
+                    response.send(e.getMessage());
+                }
+            }
+        });
+
+        server.post("/"+LINKFACILITY_URL, new HttpServerRequestCallback() {
+            @Override
+            public void onRequest(AsyncHttpServerRequest request, final AsyncHttpServerResponse response) {
+                try {
+                    assertNotNull(request.getHeaders().get("Host"));
+                    JSONObject json = new JSONObject();
+                    if (request.getBody() instanceof JSONObjectBody) {
+                        json = ((JSONObjectBody)request.getBody()).get();
+                        processLinkFacility(json);
+                    }
+                    response.send(json);
+                    // response.send(typeReceived);
+                }
+                catch (Exception e) {
+                    response.send(e.getMessage());
+                }
+            }
+        });
+        server.post("/"+SUBCOUNTY_URL, new HttpServerRequestCallback() {
+            @Override
+            public void onRequest(AsyncHttpServerRequest request, final AsyncHttpServerResponse response) {
+                try {
+                    assertNotNull(request.getHeaders().get("Host"));
+                    JSONObject json = new JSONObject();
+                    if (request.getBody() instanceof JSONObjectBody) {
+                        json = ((JSONObjectBody)request.getBody()).get();
+                        processSubCounty(json);
+                    }
+                    response.send(json);
+                }
+                catch (Exception e) {
+                    response.send(e.getMessage());
+                }
+            }
+        });
 
         server.listen(asyncServer, SERVER_PORT);
+    }
+    private void processCus(JSONObject json){
+        try{
+            JSONArray recs = json.getJSONArray(CommunityUnitTable.CU_JSON_ROOT);
+            // Get the array first JSONObject
+            for (int x = 0; x < recs.length(); x++){
+                new CommunityUnitTable(context).CuFromJson(recs.getJSONObject(x));
+            }
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+    }
+    private void processSubCounty(JSONObject json){
+        try{
+            JSONArray recs = json.getJSONArray(SubCountyTable.JSON_ROOT);
+            // Get the array first JSONObject
+            for (int x = 0; x < recs.length(); x++){
+                new SubCountyTable(context).fromJson(recs.getJSONObject(x));
+            }
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+    }
+    private void processLinkFacility(JSONObject json){
+        try{
+            JSONArray recs = json.getJSONArray(LinkFacilityTable.JSON_ROOT);
+            // Get the array first JSONObject
+            for (int x = 0; x < recs.length(); x++){
+                new LinkFacilityTable(context).fromJson(recs.getJSONObject(x));
+            }
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
+    }
+    private void processChewReferral(JSONObject json){
+        try{
+            JSONArray recs = json.getJSONArray(ChewReferralTable.JSON_ROOT);
+            // Get the array first JSONObject
+            for (int x = 0; x < recs.length(); x++){
+                new ChewReferralTable(context).fromJson(recs.getJSONObject(x));
+            }
+        }catch(JSONException e){
+            e.printStackTrace();
+        }
     }
 
     private void processRecruitments(JSONObject json){
