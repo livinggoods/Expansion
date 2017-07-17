83b2b1f4624537669363cb9685b3d6fa3615fff2
David Kimaru <kimarudg@gmail.com>
2017-07-17 16:38:54 +0300

    Sync CUs, Chews, Link Facilities and SubCounties

--- app/src/main/java/com/expansion/lg/kimaru/expansion/sync/HttpClient.java
+++ app/src/main/java/com/expansion/lg/kimaru/expansion/sync/HttpClient.java
@@ -5,16 +5,21 @@ import android.os.AsyncTask;
 import android.os.Handler;
 import android.util.Log;
 
+import com.expansion.lg.kimaru.expansion.mzigos.ChewReferral;
 import com.expansion.lg.kimaru.expansion.mzigos.Exam;
 import com.expansion.lg.kimaru.expansion.mzigos.Interview;
 import com.expansion.lg.kimaru.expansion.mzigos.Recruitment;
 import com.expansion.lg.kimaru.expansion.mzigos.Registration;
 import com.expansion.lg.kimaru.expansion.other.Constants;
 import com.expansion.lg.kimaru.expansion.other.WifiState;
+import com.expansion.lg.kimaru.expansion.tables.ChewReferralTable;
+import com.expansion.lg.kimaru.expansion.tables.CommunityUnitTable;
 import com.expansion.lg.kimaru.expansion.tables.ExamTable;
 import com.expansion.lg.kimaru.expansion.tables.InterviewTable;
+import com.expansion.lg.kimaru.expansion.tables.LinkFacilityTable;
 import com.expansion.lg.kimaru.expansion.tables.RecruitmentTable;
 import com.expansion.lg.kimaru.expansion.tables.RegistrationTable;
+import com.expansion.lg.kimaru.expansion.tables.SubCountyTable;
 import com.koushikdutta.async.http.AsyncHttpClient;
 import com.koushikdutta.async.http.AsyncHttpPost;
 import com.koushikdutta.async.http.body.JSONObjectBody;
@@ -102,6 +107,68 @@ public class HttpClient {
         return postResults;
     }
 
+    /**
+     *
+     * Added new methods to sync new data fragments
+     */
+
+    public String postSubCounties() throws Exception {
+        String postResults;
+        SubCountyTable subCountyTable = new SubCountyTable(context);
+        try {
+            postResults = this.ApiClient(subCountyTable.getJson(),
+                    SubCountyTable.JSON_ROOT, HttpServer.SUBCOUNTY_URL);
+        } catch (Exception e){
+            Log.d("ERR: Sync subCounty", e.getMessage());
+            postResults = null;
+        }
+        return postResults;
+    }
+
+    public String postLinkFacilities() throws Exception {
+        String postResults;
+        LinkFacilityTable linkFacilityTable = new LinkFacilityTable(context);
+        try {
+            postResults = this.ApiClient(linkFacilityTable.getJson(),
+                    LinkFacilityTable.JSON_ROOT, HttpServer.LINKFACILITY_URL);
+        } catch (Exception e){
+            Log.d("ERR: Sync LinkFacility", e.getMessage());
+            postResults = null;
+        }
+        return postResults;
+    }
+
+    public String postChewReferral() throws Exception {
+        String postResults;
+        ChewReferralTable chewReferralTable = new ChewReferralTable(context);
+        try {
+            postResults = this.ApiClient(chewReferralTable.getChewReferralJson(),
+                    ChewReferralTable.JSON_ROOT, HttpServer.CHEW_REFERRAL_URL);
+        } catch (Exception e){
+            Log.d("ERR: Sync LinkFacility", e.getMessage());
+            postResults = null;
+        }
+        return postResults;
+    }
+
+    public String postCommunityUnit() throws Exception {
+        String postResults;
+        CommunityUnitTable communityUnitTable = new CommunityUnitTable(context);
+        try {
+            postResults = this.ApiClient(communityUnitTable.getJson(),
+                    CommunityUnitTable.CU_JSON_ROOT, HttpServer.CU_URL);
+        } catch (Exception e){
+            Log.d("ERR: Sync LinkFacility", e.getMessage());
+            postResults = null;
+        }
+        return postResults;
+    }
+
+    /**
+     *
+     * End
+     */
+
     private String ApiClient(JSONObject json, String JsonRoot, String apiEndpoint) throws Exception {
         //  get the server URL
         AsyncHttpPost p = new AsyncHttpPost(HttpServer.SERVER_URL+":"+HttpServer.SERVER_PORT+"/"
@@ -134,9 +201,26 @@ public class HttpClient {
                 handler.post(new Runnable() {
                     @Override
                     public void run() {
-                        String registrationUrl, recruitmentUrl, examUrl, interviewUrl;
+                        String registrationUrl, recruitmentUrl, examUrl, interviewUrl,
+                                linkFacilityUrl, chewReferralUrl, communityUnitUrl, subCountyUrl;
                         WifiState wifiState = new WifiState(context);
                         if (wifiState.canReachPeerServer()) {
+                            //Subcounty
+                            subCountyUrl = url + "/" + HttpServer.SUBCOUNTY_URL;
+                            new ProcessSubCounty().execute(subCountyUrl);
+
+                            //Link Favilities
+                            linkFacilityUrl = url + "/" + HttpServer.LINKFACILITY_URL;
+                            new ProcessLinkFacility().execute(linkFacilityUrl);
+
+                            //chews
+                            chewReferralUrl = url + "/" + HttpServer.CHEW_REFERRAL_URL;
+                            new ProcessChewReferral().execute(chewReferralUrl);
+
+                            //CUs
+                            communityUnitUrl = url + "/" + HttpServer.CU_URL;
+                            new ProcessCommunityUnit().execute(communityUnitUrl);
+
                             //Recruitments
                             recruitmentUrl = url + "/" + HttpServer.RECRUIRMENT_URL;
                             new ProcessRecruitment().execute(recruitmentUrl);
@@ -167,23 +251,36 @@ public class HttpClient {
         protected String doInBackground(String... strings){
             try {
                 String status = postRecruitments();
-            } catch (Exception e) {
-            }
+            } catch (Exception e) {}
 
             try {
                 String status = postRegistrations();
-            } catch (Exception e) {
-            }
+            } catch (Exception e) {}
 
             try {
                 String status = postExams();
-            } catch (Exception e) {
-            }
+            } catch (Exception e) {}
 
             try {
                 String status = postInterviews();
-            } catch (Exception e) {
-            }
+            } catch (Exception e) {}
+
+            try {
+                String status = postCommunityUnit();
+            } catch (Exception e) {}
+
+            try {
+                String status = postChewReferral();
+            } catch (Exception e) {}
+
+            try {
+                String status = postLinkFacilities();
+            } catch (Exception e) {}
+
+            try {
+                String status = postSubCounties();
+            } catch (Exception e) {}
+
             return "";
         }
     }
@@ -289,6 +386,35 @@ public class HttpClient {
         } // onPostExecute() end
     } // ProcessJSON class end
 
+    private class ProcessSubCounty extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... strings){
+            String stream = null;
+            String urlString = strings[0];
+
+            ApiClient hh = new ApiClient();
+            stream = hh.GetHTTPData(urlString);
+            if(stream !=null){
+                try{
+                    // Get the full HTTP Data as JSONObject
+                    JSONObject reader= new JSONObject(stream);
+                    JSONArray recs = reader.getJSONArray(SubCountyTable.JSON_ROOT);
+                    for (int x = 0; x < recs.length(); x++){
+                        new SubCountyTable(context).fromJson(recs.getJSONObject(x));
+                    }
+                }catch(JSONException e){
+                    e.printStackTrace();
+                }
+
+            }
+            // Return the data from specified url
+            return stream;
+        }
+
+        protected void onPostExecute(String stream){
+            // if statement end
+        } // onPostExecute() end
+    }
+
     private class ProcessInterviews extends AsyncTask<String, Void, String> {
         protected String doInBackground(String... strings){
             String stream = null;
@@ -321,6 +447,98 @@ public class HttpClient {
         } // onPostExecute() end
     }
 
+    private class ProcessLinkFacility extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... strings){
+            String stream = null;
+            String urlString = strings[0];
+
+            ApiClient hh = new ApiClient();
+            stream = hh.GetHTTPData(urlString);
+            if(stream !=null){
+                try{
+                    // Get the full HTTP Data as JSONObject
+                    JSONObject reader= new JSONObject(stream);
+
+                    // Get the JSONArray recruitments
+                    JSONArray recs = reader.getJSONArray(LinkFacilityTable.JSON_ROOT);
+
+                    for (int x = 0; x < recs.length(); x++){
+                        new LinkFacilityTable(context).fromJson(recs.getJSONObject(x));
+                    }
+                }catch(JSONException e){
+                    e.printStackTrace();
+                }
+            }
+            // Return the data from specified url
+            return stream;
+        }
+
+        protected void onPostExecute(String stream){
+            // if statement end
+        }
+    }
+
+
+    private class ProcessChewReferral extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... strings){
+            String stream = null;
+            String urlString = strings[0];
+
+            ApiClient hh = new ApiClient();
+            stream = hh.GetHTTPData(urlString);
+            if(stream !=null){
+                try{
+                    // Get the full HTTP Data as JSONObject
+                    JSONObject reader= new JSONObject(stream);
+
+                    // Get the JSONArray recruitments
+                    JSONArray recs = reader.getJSONArray(ChewReferralTable.JSON_ROOT);
+
+                    for (int x = 0; x < recs.length(); x++){
+                        new ChewReferralTable(context).fromJson(recs.getJSONObject(x));
+                    }
+                }catch(JSONException e){
+                    e.printStackTrace();
+                }
+            }
+            // Return the data from specified url
+            return stream;
+        }
+
+        protected void onPostExecute(String stream){
+            // if statement end
+        }
+    }
+
+
+    private class ProcessCommunityUnit extends AsyncTask<String, Void, String> {
+        protected String doInBackground(String... strings){
+            String stream = null;
+            String urlString = strings[0];
+
+            ApiClient hh = new ApiClient();
+            stream = hh.GetHTTPData(urlString);
+            if(stream !=null){
+                try{
+                    // Get the full HTTP Data as JSONObject
+                    JSONObject reader= new JSONObject(stream);
+                    JSONArray recs = reader.getJSONArray(CommunityUnitTable.CU_JSON_ROOT);
+
+                    for (int x = 0; x < recs.length(); x++){
+                        new CommunityUnitTable(context).CuFromJson(recs.getJSONObject(x));
+                    }
+                }catch(JSONException e){
+                    e.printStackTrace();
+                }
+            }
+            // Return the data from specified url
+            return stream;
+        }
+
+        protected void onPostExecute(String stream){
+            // if statement end
+        }
+    }
 
     //we add method to pull users from the
 
