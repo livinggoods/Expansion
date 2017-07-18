50276bc28d5769bda2365add4ffcc317affda866
David Kimaru <kimarudg@gmail.com>
2017-07-18 13:09:59 +0300

    Create a method to pull cloud details globally

--- app/src/main/java/com/expansion/lg/kimaru/expansion/other/CloudConnection.java
+++ app/src/main/java/com/expansion/lg/kimaru/expansion/other/CloudConnection.java
@@ -1,8 +1,23 @@
 package com.expansion.lg.kimaru.expansion.other;
 
+import android.content.Context;
+
+import com.expansion.lg.kimaru.expansion.activity.SessionManagement;
+
 /**
  * Created by kimaru on 7/18/17.
  */
 
 public class CloudConnection {
+    Context context;
+
+    public CloudConnection(Context mContext){
+        this.context = mContext;
+    }
+
+    public String ServerUrl(){
+        SessionManagement sessionManagement = new SessionManagement(context);
+        String serverUrl = sessionManagement.getCloudUrl();
+        return serverUrl;
+    }
 }
