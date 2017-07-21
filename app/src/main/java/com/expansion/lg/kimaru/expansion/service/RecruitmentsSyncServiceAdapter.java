0abfba8c50a5de8726cace42845b1fe9109dda3c
David Kimaru <kimarudg@gmail.com>
2017-07-21 12:41:30 +0300

    No need to sync sub counties to the Cloud,   the reverse is true

--- app/src/main/java/com/expansion/lg/kimaru/expansion/service/RecruitmentsSyncServiceAdapter.java
+++ app/src/main/java/com/expansion/lg/kimaru/expansion/service/RecruitmentsSyncServiceAdapter.java
@@ -50,7 +50,6 @@ public class RecruitmentsSyncServiceAdapter extends AbstractThreadedSyncAdapter 
         client.syncCommunityUnits();
         client.syncReferrals();
         client.syncLinkFacilities();
-        client.syncSubCounties();
 
     }
 
