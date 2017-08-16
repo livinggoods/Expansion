3252544aa04efc41ad6e382fad92ce4c202723ef
David Kimaru <kimarudg@gmail.com>
2017-08-16 15:59:53 +0300

    Add partner model and allow syncing

--- app/src/main/java/com/expansion/lg/kimaru/expansion/mzigos/Partners.java
+++ app/src/main/java/com/expansion/lg/kimaru/expansion/mzigos/Partners.java
@@ -4,92 +4,130 @@ package com.expansion.lg.kimaru.expansion.mzigos;
  * Created by kimaru on 3/28/17.
  */
 
+/**
+ *
+ */
+
 public class Partners {
-    String partnerID;
-    String partnerName;
-    boolean doingIccm;
-    String iccmComponent;
-    boolean doingMHealth;
-    String comment;
-    Long dateAdded;
-    Long addedBy;
+    String partnerID, partnerName, contactPerson, contactPersonPhone,
+            parent, mappingId, country, comment;
+    boolean synced, archived;
+    Long dateAdded, addedBy;
 
     //empty constructor
     public Partners(){}
 
-    public Partners (String partnerID, String partnerName, String iccmComponent,
-                     boolean doingIccm, boolean doingMHealth, String comment, Long dateAdded, Long addedBy){
+    public Partners(String partnerID, String partnerName, String contactPerson,
+                    String contactPersonPhone, String parent, String mappingId,
+                    String country, String comment, boolean synced, boolean archived,
+                    Long dateAdded, Long addedBy) {
         this.partnerID = partnerID;
-        this.doingIccm = doingIccm;
         this.partnerName = partnerName;
-        this.iccmComponent = iccmComponent;
-        this.doingMHealth = doingMHealth;
+        this.contactPerson = contactPerson;
+        this.contactPersonPhone = contactPersonPhone;
+        this.parent = parent;
+        this.mappingId = mappingId;
+        this.country = country;
         this.comment = comment;
-        this.addedBy = addedBy;
+        this.synced = synced;
+        this.archived = archived;
         this.dateAdded = dateAdded;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public void setDoingIccm(boolean doingIccm) {
-        this.doingIccm = doingIccm;
-    }
-
-    public void setDoingMHealth(boolean doingMHealth) {
-        this.doingMHealth = doingMHealth;
-    }
-
-    public void setIccmComponent(String iccmComponent) {
-        this.iccmComponent = iccmComponent;
-    }
-
-    public void setPartnerName(String partnerName) {
-        this.partnerName = partnerName;
+        this.addedBy = addedBy;
     }
 
     public String getPartnerID() {
         return partnerID;
     }
 
-    public String getComment() {
-        return comment;
-    }
-
-    public boolean isDoingIccm() {
-        return doingIccm;
-    }
-
-    public boolean isDoingMHealth() {
-        return doingMHealth;
-    }
-
-    public String getIccmComponent() {
-        return iccmComponent;
-    }
-
     public String getPartnerName() {
         return partnerName;
     }
 
-    public void setPartnerID(String partnerID) {
-        this.partnerID = partnerID;
+    public String getContactPerson() {
+        return contactPerson;
     }
 
-    public Long getAddedBy() {
-        return addedBy;
+    public String getContactPersonPhone() {
+        return contactPersonPhone;
+    }
+
+    public String getParent() {
+        return parent;
+    }
+
+    public String getMappingId() {
+        return mappingId;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public boolean isSynced() {
+        return synced;
+    }
+
+    public boolean isArchived() {
+        return archived;
     }
 
     public Long getDateAdded() {
         return dateAdded;
     }
 
-    public void setAddedBy(Long addedBy) {
-        this.addedBy = addedBy;
+    public Long getAddedBy() {
+        return addedBy;
+    }
+
+    public void setPartnerID(String partnerID) {
+        this.partnerID = partnerID;
+    }
+
+    public void setPartnerName(String partnerName) {
+        this.partnerName = partnerName;
+    }
+
+    public void setContactPerson(String contactPerson) {
+        this.contactPerson = contactPerson;
+    }
+
+    public void setContactPersonPhone(String contactPersonPhone) {
+        this.contactPersonPhone = contactPersonPhone;
+    }
+
+    public void setParent(String parent) {
+        this.parent = parent;
+    }
+
+    public void setMappingId(String mappingId) {
+        this.mappingId = mappingId;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public void setSynced(boolean synced) {
+        this.synced = synced;
+    }
+
+    public void setArchived(boolean archived) {
+        this.archived = archived;
     }
 
     public void setDateAdded(Long dateAdded) {
         this.dateAdded = dateAdded;
+    }
+
+    public void setAddedBy(Long addedBy) {
+        this.addedBy = addedBy;
     }
 }
