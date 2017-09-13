761f4895e6c1575249f2c217a2a301dc67c627d1
David Kimaru <kimarudg@gmail.com>
2017-09-13 13:20:02 +0300

    Add links to home Fragment

--- app/src/main/java/com/expansion/lg/kimaru/expansion/fragment/HomeFragment.java
+++ app/src/main/java/com/expansion/lg/kimaru/expansion/fragment/HomeFragment.java
@@ -2,6 +2,7 @@ package com.expansion.lg.kimaru.expansion.fragment;
 /**
  * Created by kimaru on 3/11/17.
  */
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -9,18 +10,24 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.expansion.lg.kimaru.expansion.R;
 import com.expansion.lg.kimaru.expansion.activity.HttpServerActivity;
 import com.expansion.lg.kimaru.expansion.activity.MainActivity;
 import com.expansion.lg.kimaru.expansion.activity.SessionManagement;
 import com.expansion.lg.kimaru.expansion.mzigos.Recruitment;
+import com.expansion.lg.kimaru.expansion.other.Constants;
+import com.expansion.lg.kimaru.expansion.other.InternetCheck;
+import com.expansion.lg.kimaru.expansion.sync.TrainingDataSync;
 
 import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -41,10 +48,13 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     private String mParam2;
     private Handler mHandler;
 
+    boolean internetAvailable;
+
     Recruitment recruitment;
 
 
-    Button mapping, recruitments, registrations, exams, interviews, sharing;
+    Button mapping, recruitments, registrations, exams, interviews, sharing, graduation,
+            training, cloud;
     SessionManagement sessionManagement;
     HashMap <String, String> user;
     String country;
@@ -93,6 +103,8 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         user = sessionManagement.getUserDetails();
         country = sessionManagement.getUserDetails().get(SessionManagement.KEY_USER_COUNTRY);
 
+        //
+
         //mapping, recruitments, registrations, exams, interviews, sharing
         View v = inflater.inflate(R.layout.fragment_home, container, false);
         mapping = (Button) v.findViewById(R.id.btnMapping);
@@ -101,6 +113,10 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         exams = (Button) v.findViewById(R.id.btnExams);
         interviews = (Button) v.findViewById(R.id.btnInterviews);
         sharing = (Button) v.findViewById(R.id.btnSharing);
+        graduation = (Button) v.findViewById(R.id.btnGraduation);
+        training = (Button) v.findViewById(R.id.btnTraining);
+        cloud = (Button) v.findViewById(R.id.btnCloud);
+
 
         mapping.setOnClickListener(this);
         recruitments.setOnClickListener(this);
@@ -108,6 +124,9 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         exams.setOnClickListener(this);
         interviews.setOnClickListener(this);
         sharing.setOnClickListener(this);
+        graduation.setOnClickListener(this);
+        training.setOnClickListener(this);
+        cloud.setOnClickListener(this);
 
 
         return v;
@@ -123,12 +142,6 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
     }
 
     @Override
@@ -197,6 +210,45 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
                 fragmentTransaction.commitAllowingStateLoss();
                 break;
 
+            case R.id.btnTraining:
+                try {
+                    if (new InternetCheck(getContext()).isConnected()){
+                        Toast.makeText(getContext(), "Internet is connnected", Toast.LENGTH_SHORT).show();
+                        // try to update the content
+                        final ProgressDialog progressDialog = new ProgressDialog(getContext());
+                        progressDialog.setTitle("Syncing Trainings");
+                        progressDialog.setMessage("Please wait ...");
+                        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                        progressDialog.setCancelable(false);
+                        progressDialog.show();
+                        new Thread(new Runnable() {
+                            @Override
+                            public void run() {
+                                try {
+                                    //TimeUnit.MINUTES.sleep(1);
+                                    TimeUnit.SECONDS.sleep(5);
+                                    Log.d("Tremap", "~~~~~~~~~~~~~~~~~~STARTING~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+                                    TrainingDataSync trainingDataSync = new TrainingDataSync(getContext());
+                                    trainingDataSync.pollNewTrainings();
+
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }finally {
+                                    progressDialog.dismiss();
+                                }
+                            }
+                        }).start();
+                    }
+                }catch (Exception e){}
+                fragment = new TrainingsFragment();
+                fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
+                fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                        android.R.anim.fade_out);
+                fragmentTransaction.replace(R.id.frame, fragment, MainActivity.TAG_NEW_RECRUITMENT);
+                fragmentTransaction.commitAllowingStateLoss();
+
+                break;
+
             case R.id.btnInterviews:
                 //
                 if (sessionManagement.isRecruitmentSet()){
@@ -229,6 +281,9 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
             case R.id.btnSharing:
                 startActivity(new Intent(getActivity(), HttpServerActivity.class));
                 break;
+            case R.id.btnCloud:
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.CLOUD_ADDRESS));
+                startActivity(browserIntent);
 
         }
     }
@@ -246,5 +301,12 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // register connection status listener
+        // AppLaunch.getInstance().setConnectivityListener(this);
     }
 }
