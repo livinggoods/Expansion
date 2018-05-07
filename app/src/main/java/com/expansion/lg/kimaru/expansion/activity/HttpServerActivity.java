9bcac21ff3e25fd4ec4653d336ffd17105cfcab9
David Kimaru <kimarudg@gmail.com>
2018-05-07 13:25:02 +0300

    Server feedback on async tasks

--- app/src/main/java/com/expansion/lg/kimaru/expansion/activity/HttpServerActivity.java
+++ app/src/main/java/com/expansion/lg/kimaru/expansion/activity/HttpServerActivity.java
@@ -1,8 +1,11 @@
 package com.expansion.lg.kimaru.expansion.activity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.text.util.Linkify;
 import android.util.Log;
@@ -16,22 +19,30 @@ import com.expansion.lg.kimaru.expansion.mzigos.Exam;
 import com.expansion.lg.kimaru.expansion.mzigos.Interview;
 import com.expansion.lg.kimaru.expansion.mzigos.Recruitment;
 import com.expansion.lg.kimaru.expansion.mzigos.Registration;
+import com.expansion.lg.kimaru.expansion.other.Constants;
 import com.expansion.lg.kimaru.expansion.other.ExportDataToCsv;
 import com.expansion.lg.kimaru.expansion.sync.ApiClient;
 import com.expansion.lg.kimaru.expansion.sync.HttpClient;
 import com.expansion.lg.kimaru.expansion.sync.HttpServer;
 import com.expansion.lg.kimaru.expansion.sync.IccmDataSync;
 import com.expansion.lg.kimaru.expansion.sync.LocationDataSync;
+import com.expansion.lg.kimaru.expansion.sync.UserDataSync;
+import com.expansion.lg.kimaru.expansion.tables.CountyLocationTable;
 import com.expansion.lg.kimaru.expansion.tables.ExamTable;
 import com.expansion.lg.kimaru.expansion.tables.InterviewTable;
+import com.expansion.lg.kimaru.expansion.tables.ParishTable;
 import com.expansion.lg.kimaru.expansion.tables.RecruitmentTable;
 import com.expansion.lg.kimaru.expansion.tables.RegistrationTable;
+import com.expansion.lg.kimaru.expansion.tables.SubCountyTable;
+import com.expansion.lg.kimaru.expansion.tables.WardTable;
+import com.ontbee.legacyforks.cn.pedant.SweetAlert.SweetAlertDialog;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -42,8 +53,14 @@ public class HttpServerActivity extends AppCompatActivity implements View.OnClic
 
 
 
-    Button enableServer, buttonGetSubCounty;
-    Button resyncLocations, shareRecords, syncIccmComponents, buttonExportData;
+    Button enableServer, buttonGetSubCounty, recreateLocationTable;
+    Button resyncLocations, shareRecords, syncIccmComponents;
+    SweetAlertDialog pDialog;
+
+    Context context;
+    SessionManagement session;
+    private LocationDataSync locationDataSync;
+    ProgressDialog progressDialog;
 
 
 
@@ -52,20 +69,24 @@ public class HttpServerActivity extends AppCompatActivity implements View.OnClic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_http_server);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        context = this;
+        session = new SessionManagement(context);
+        locationDataSync = new LocationDataSync(getBaseContext());
 
         enableServer = (Button) findViewById(R.id.buttonStartServer);
         resyncLocations = (Button) findViewById(R.id.buttonLocationSync);
         shareRecords = (Button) findViewById(R.id.buttonShareRecords);
         buttonGetSubCounty = (Button) findViewById(R.id.buttonGetSubCounty);
         syncIccmComponents = (Button) findViewById(R.id.iccmComponents);
-        buttonExportData = (Button) findViewById(R.id.buttonExportData);
+        recreateLocationTable = (Button) findViewById(R.id.recreateLocationTable);
+        progressDialog = new ProgressDialog(context);
 
         enableServer.setOnClickListener(this);
         shareRecords.setOnClickListener(this);
         resyncLocations.setOnClickListener(this);
         buttonGetSubCounty.setOnClickListener(this);
         syncIccmComponents.setOnClickListener(this);
-        buttonExportData.setOnClickListener(this);
+        recreateLocationTable.setOnClickListener(this);
 
     }
 
@@ -88,6 +109,7 @@ public class HttpServerActivity extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View view){
+        MainActivity mainActivity = new MainActivity();
         switch (view.getId()){
             case R.id.buttonStartServer:
                 HttpServer server = new HttpServer(getBaseContext());
@@ -107,43 +129,11 @@ public class HttpServerActivity extends AppCompatActivity implements View.OnClic
 
                 break;
             case R.id.buttonLocationSync:
-                Thread thread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        LocationDataSync locationDataSync = new LocationDataSync(getBaseContext());
-                        locationDataSync.pollLocations();
-                    }
-                });
-                thread.start();
+                syncLocationsInForeGround();
                 break;
 
             case R.id.buttonGetSubCounty:
-                Toast.makeText(getBaseContext(), "Starting", Toast.LENGTH_SHORT).show();
-                Thread locationThread = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        LocationDataSync locationDataSync = new LocationDataSync(getBaseContext());
-                        locationDataSync.getKeSubcounties();
-                    }
-                });
-                locationThread.start();
-                Toast.makeText(getBaseContext(), "Sync started", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.buttonExportData:
-                Toast.makeText(getBaseContext(), "Exporting data", Toast.LENGTH_SHORT).show();
-                Thread dataExport = new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        ExportDataToCsv exportDataToCsv = new ExportDataToCsv(getApplicationContext());
-                        exportDataToCsv.exportChewReferral();
-                        exportDataToCsv.exportCommunityUnit();
-                        exportDataToCsv.exportMobilization();
-                        exportDataToCsv.exportVillage();
-                        exportDataToCsv.exportParish();
-                        exportDataToCsv.exportMapping();
-                    }
-                });
-                dataExport.start();
+                syncKeWardsInForeground();
                 break;
 
             case R.id.iccmComponents:
@@ -157,8 +147,11 @@ public class HttpServerActivity extends AppCompatActivity implements View.OnClic
                 iccmThread.start();
                 Toast.makeText(getBaseContext(), "Getting ICCM components ", Toast.LENGTH_SHORT).show();
                 break;
-
-
+            case R.id.recreateLocationTable:
+                CountyLocationTable countyLocationTable = new CountyLocationTable(getBaseContext());
+                countyLocationTable.reCreateDb();
+                syncLocationsInForeGround();
+                break;
         }
     }
 
@@ -166,6 +159,166 @@ public class HttpServerActivity extends AppCompatActivity implements View.OnClic
     @Override
     public void onResume (){
         super.onResume();
+    }
+
+
+
+    public void syncKeWardsInForeground(){
+        new syncKeWardsInForeground().execute(new Constants(getBaseContext()).getCloudAddress() + "/api/v1/sync/ke-counties");
+    }
+    private class syncKeWardsInForeground extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            pDialog = new SweetAlertDialog(context, SweetAlertDialog.PROGRESS_TYPE)
+                    .setTitleText("Loading KE Locations. Please wait");
+            pDialog.show();
+            pDialog.setCancelable(false);
+        }
+
+        protected String doInBackground(String... strings){
+            String stream = null;
+            String urlString = strings[0];
+            ApiClient hh = new ApiClient();
+            stream = hh.GetHTTPData(urlString);
+            if(stream !=null){
+                try{
+                    JSONObject reader= new JSONObject(stream);
+                    JSONArray recs = reader.getJSONArray("counties");
+                    SubCountyTable subCountyTable = new SubCountyTable(context);
+                    for (int x = 0; x < recs.length(); x++){
+                        // I have the county Details, lets extract all subcounties
+                        JSONObject county = recs.getJSONObject(x);
+                        JSONArray subCounties = county.getJSONArray("subcounties");
+                        for (int a = 0; a<subCounties.length(); a++){
+                            // I have the subcounty, create the subcounty
+                            JSONObject subCounty = subCounties.getJSONObject(a);
+                            new SubCountyTable(context).fromJson(subCounty);
+                            //also get the wards
+                            JSONArray wards = subCounty.getJSONArray("wards");
+                            for (int b=0; b<wards.length(); b++){
+                                JSONObject ward = wards.getJSONObject(b);
+                                //create a ward
+                                new WardTable(context).fromJson(ward);
+                            }
+                        }
+                    }
+                }catch(JSONException e){
+                    Log.d("TREMAP", "KE County Sync ERROR "+e.getMessage());
+                }
+
+            }
+            return stream;
+        }
+        protected void onPostExecute(String stream){
+            session.flagSynced(false); // just to force the app to resync the data.
+            pDialog.dismiss();
+        } // onPostExecute() end
+    }
+
+
+
+
+    public void syncLocationsInForeGround(){
+        new syncLocationsInForeGround().execute(new Constants(getBaseContext()).getCloudAddress() + "/api/v1/sync/locations");
+    }
+    private class syncLocationsInForeGround extends AsyncTask<String, HashMap, String> {
+        Integer totalRecords = 0;
+        Integer processedRecords = 0;
+        String recordType = "";
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+            recordType= "Location";
+            progressDialog.setTitle("Syncing "+ recordType);
+            progressDialog.setMessage("Please wait ...");
+            progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+            progressDialog.setCancelable(false);
+            progressDialog.show();
+        }
+
+        protected String doInBackground(String... strings){
+            String stream = null;
+            HashMap<String, String> progress = new HashMap<String, String>();
+            String urlString = strings[0];
+            ApiClient hh = new ApiClient();
+            stream = hh.GetHTTPData(urlString);
+            if(stream !=null){
+                try{
+                    JSONObject reader= new JSONObject(stream);
+                    JSONArray recs = reader.getJSONArray("locations");
+                    CountyLocationTable countyLocationTable = new CountyLocationTable(getBaseContext());
+                    totalRecords = recs.length();
+                    for (int x = 0; x < recs.length(); x++){
+                        countyLocationTable.fromJson(recs.getJSONObject(x));
+                        processedRecords = x;
+                        progress = new HashMap<String, String>();
+                        progress.put("total", totalRecords.toString());
+                        progress.put("processed", processedRecords.toString());
+                        progress.put("type", "Location");
+                        publishProgress(progress);
+                    }
+                }catch(JSONException e){
+                }
+            }
+            recordType= "Parish";
+            processedRecords = 0;
+            progress = new HashMap<String, String>();
+            progress.put("total", "100");
+            progress.put("processed", "0");
+            progress.put("type", "Parish");
+            progress.put("message", "Downloading parishes from the Cloud ... ");
+            progress.put("title", "Getting Parishes");
+            progress.put("action", "Downloading");
+            publishProgress(progress);
+
+            String parishStream = hh.GetHTTPData(new Constants(context).getCloudAddress()+"/api/v1/sync/parish");
+            if(parishStream !=null){
+                try{
+                    JSONObject reader= new JSONObject(parishStream);
+                    JSONArray recs = reader.getJSONArray("parish");
+                    ParishTable parishTable = new ParishTable(context);
+                    totalRecords = recs.length();
+                    for (int y = 0; y < recs.length(); y++){
+                        JSONObject parish = recs.getJSONObject(y);
+                        progress = new HashMap<String, String>();
+                        progress.put("total", totalRecords.toString());
+                        progress.put("processed", String.valueOf(y));
+                        progress.put("type", "Parish");
+                        progress.put("message", "Processing parishes ... ");
+                        progress.put("title", "Processing Parish");
+                        publishProgress(progress);
+                        parishTable.fromJson(parish);
+                    }
+                }catch(JSONException e){
+                    Log.d("TREMAP", "ERROR Creating parish:\n "+e.getMessage());
+                }
+            }
+            return stream;
+
+        }
+        protected void onPostExecute(String stream){
+            session.flagSynced(false); // just to force the app to resync the data.
+            progressDialog.dismiss();
+        } // onPostExecute() end
+
+        @Override
+        protected void onProgressUpdate(HashMap... updates){
+            super.onProgressUpdate(updates);
+            HashMap<String, String> progress = updates[0];
+            if (progress.containsKey("action")){
+                progressDialog.setProgress(0);
+                progressDialog.setMax(100);
+                progressDialog.setMessage(progress.get("message"));
+                progressDialog.setTitle(progress.get("title"));
+            }else{
+                progressDialog.setProgress(Integer.valueOf(progress.get("processed")));
+                progressDialog.setMax(Integer.valueOf(progress.get("total")));
+                progressDialog.setMessage("Syncing "+ progress.get("type") +"\n Please wait ... ");
+                progressDialog.setTitle("Syncing "+ progress.get("type"));
+            }
+
+        }
     }
 
 }
